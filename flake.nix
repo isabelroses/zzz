39d200eb14c651f07ce0826ecfc6397e04831cff
comfysage <67917529+comfysage@users.noreply.github.com>
2024-05-30 10:31:01 +0200

    feat: create home-manager module

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgsForEach = nixpkgs.legacyPackages;
@@ -13,5 +17,7 @@
     devShells = forEachSystem (system: {
       default = pkgsForEach.${system}.callPackage ./shell.nix {};
     });
+
+    homeManagerModules.default = import ./hm-module.nix self;
   };
 }
